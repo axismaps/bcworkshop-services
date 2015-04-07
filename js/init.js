0abe9074c09bec7ca190e88c6106164f9d1909c6
David Heyman <dave@axismaps.com>
2015-04-07 11:31:59 +0100

    Moved layers code

--- js/init.js
+++ js/init.js
@@ -1,6 +1,8 @@
 var map,
 	neighborhoods,
 	template,
+	overlays,
+	labels,
 	selected = {},
 	endpoint = window.location.origin + ':3000';
 
@@ -65,8 +67,57 @@ function init_map(){
 			$( '#about .close').show();
 		})
 		.addTo( map );
-		
+}
+
+function init_layers( button ) {
+	//init layer group to store labels
+	labels = L.layerGroup().addTo( map );
 	
+	//init layer group to store overlays
+	overlays = L.layerGroup().addTo( map );
+	
+	//hides the loader icon on first view
+	$( '#dropdown-toggle i' ).toggle();
+	
+	//button should be a jquery object
+	button.append( '<li role="presentation"><label><input type="radio" name="layers" value="" checked>None</label></li>' );
+	_.each( layers, function( layer ) {
+		var $li = $( '<li role="presentation"><label><input type="radio" name="layers" value="' + layer.table + '"' + ( layer.default ? ' checked="true"' : '' ) +'>' + layer.name + '</label></li>' );
+		$li.data( layer );
+		button.append( $li );
+	});
+	
+	button.find( 'input' ).click( function() {
+		overlays.clearLayers();
+		labels.clearLayers();
+		
+		if( $( this ).val() != '' ) {
+			$( "#dropdown-toggle" ).children().toggle();
+			
+			var layerStyle = L.geoJson( null, {
+				style : function( feature ) {
+					return { 
+						color : '#ed2a24',
+						fillOpacity : 0,
+						pointerEvents : 'none'
+					};
+		    	}
+			});
+			
+			$( "#dropdown-toggle" ).css( "pointer-events", "none" );
+			
+			if( $( this ).parents( "li" ).data().labels ) {
+  				  L.tileLayer( $( this ).parents( "li" ).data().labels ).addTo( labels );
+		  }
+			
+			omnivore.topojson( endpoint + "/topojson/" + $( this ).val(), null, layerStyle ).addTo( overlays ).on( 'ready', function() {
+				$( "#dropdown-toggle" ).children().toggle();
+				$( "#dropdown-toggle" ).css( "pointer-events", "auto" );
+			}); 
+		}
+		map.getContainer().focus();
+	});	
+	button.find( 'input:checked' ).trigger( 'click' );
 }
 
 function highlightFeature( e ) {
