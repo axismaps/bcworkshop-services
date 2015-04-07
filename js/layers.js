54e0f9020f2edebca96e59cefecb413ade6c5710
David Heyman <dave@axismaps.com>
2015-04-07 11:32:46 +0100

    Labels for layers

--- js/layers.js
+++ js/layers.js
@@ -1,18 +1,20 @@
-var overlays;
-	layers = [
+var layers = [
 		{
 			"table" : "city_council",
 			"name" : "City Council Districts",
+			"labels" : 'https://{s}.tiles.mapbox.com/v4/bcworkshop12.fd1a0343/{z}/{x}/{y}.png?access_token=' + accessToken,
 			"default": false
 		},
 		{
 			"table" : "school_districts",
 			"name" : "School Districts",
+			"labels" : 'https://{s}.tiles.mapbox.com/v4/bcworkshop12.c359fa37/{z}/{x}/{y}.png?access_token=' + accessToken,
 			"default": false
 		},
 		{
 			"table" : "zips",
 			"name" : "Zip Codes",
+			"labels" : 'https://{s}.tiles.mapbox.com/v4/bcworkshop12.41469e8d/{z}/{x}/{y}.png?access_token=' + accessToken,
 			"default": false
 		},
 		{
@@ -21,3 +23,4 @@ var overlays;
 			"default" : false
 		}
 	];
+	
